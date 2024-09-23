7568e2a67d494edce07977f29a0c9e9ff9200244
kumar10248 <kumardevashish000@gmail.com>
2024-09-24 00:50:35 +0530

    Refactor transaction history layout and styles

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -14,26 +14,34 @@ const Page = () => {
         <Button className="bg-amber-500 font-bold text-2xl h-[60px] ml-7 mt-3">Send Money</Button>
         <Button className="bg-amber-500 font-bold text-2xl h-[60px] ml-7 mt-3">Receive Money</Button>
       </div>
+
       <h1 className=" text-3xl mt-6 ml-5">Transaction History:</h1>
       <hr className=" ml-5 mr-5 border-t-2 border-gray-400" />
+
       <h1 className=" text-2xl mt-6 ml-5">name</h1>
       <h1 className=" text-2xl mt-1 ml-5">date</h1>
       <hr className=" ml-5 mr-5 border-t-2 border-gray-400 mt-3" />
+
       <h1 className=" text-2xl mt-6 ml-5">name</h1>
       <h1 className=" text-2xl mt-1 ml-5">date</h1>
       <hr className=" ml-5 mr-5 border-t-2 border-gray-400 mt-3" />
+
       <h1 className=" text-2xl mt-6 ml-5">name</h1>
       <h1 className=" text-2xl mt-1 ml-5">date</h1>
       <hr className=" ml-5 mr-5 border-t-2 border-gray-400 mt-3" />
+
       <h1 className=" text-2xl mt-6 ml-5">name</h1>
       <h1 className=" text-2xl mt-1 ml-5">date</h1>
       <hr className=" ml-5 mr-5 border-t-2 border-gray-400 mt-3" />
+
       <h1 className=" text-2xl mt-6 ml-5">name</h1>
       <h1 className=" text-2xl mt-1 ml-5">date</h1>
       <hr className=" ml-5 mr-5 border-t-2 border-gray-400 mt-3" />
+
       <h1 className=" text-2xl mt-6 ml-5">name</h1>
       <h1 className=" text-2xl mt-1 ml-5">date</h1>
       <hr className=" ml-5 mr-5 border-t-2 border-gray-400 mt-3" />
+
       <h1 className=" text-2xl mt-6 ml-5">name</h1>
       <h1 className=" text-2xl mt-1 ml-5">date</h1>
       <hr className=" ml-5 mr-5 border-t-2 border-gray-400 mt-3" />
