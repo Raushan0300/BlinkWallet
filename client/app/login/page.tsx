cc7ca4f93c5e1f98d0b50c2b08657de12501b2b2
Raushan Kumar <raushankumar2909@gmail.com>
2024-09-23 22:48:27 +0530

    Refactor tailwind configuration and add utility function

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -2,31 +2,32 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
+
 const page = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-        <Tabs defaultValue="login" className="w-[400px] bg-black rounded p-2">
-  <TabsList className="grid w-full grid-cols-2 bg-neutral-700 rounded">
-    <TabsTrigger value="login" className="text-white rounded data-[state=active]:bg-black data-[state=active]:text-white data-[state=inactive]:hover:underline">Login</TabsTrigger>
-    <TabsTrigger value="signup" className="text-white rounded data-[state=active]:bg-black data-[state=active]:text-white data-[state=inactive]:hover:underline">Sign Up</TabsTrigger>
-  </TabsList>
-  <TabsContent value="login" className="text-white flex flex-col gap-2">
-    <h1 className="text-2xl">Login</h1>
-    <Input type="email" placeholder="Email" className="placeholder:text-slate-400 rounded" />
-    <Input type="password" placeholder="Password" className="placeholder:text-slate-400 rounded" />
-    <Button className="rounded bg-white text-black hover:text-white hover:bg-[#FF9505]">Login</Button>
-  </TabsContent>
-  <TabsContent value="signup" className="text-white flex flex-col gap-2">
-    <h1 className="text-2xl">Signup</h1>
-    <Input type="text" placeholder="Name" className="placeholder:text-slate-400 rounded" />
-    <Input type="email" placeholder="Email" className="placeholder:text-slate-400 rounded" />
-    <Input type="password" placeholder="Password" className="placeholder:text-slate-400 rounded" />
-    <Button className="rounded bg-white text-black hover:text-white hover:bg-[#FF9505]">Signup</Button>
-  </TabsContent>
-</Tabs>
-
+    <div className="flex flex-col justify-center items-center h-screen">
+      <Tabs
+        defaultValue="login"
+        className="w-[400px]">
+        <TabsList className="w-full">
+          <TabsTrigger value="login" className="w-full font-bold">Login</TabsTrigger>
+          <TabsTrigger value="signup" className="w-full font-bold">Sign Up</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login" className="flex flex-col gap-3">
+         <Input placeholder="Email" type="email" />
+            <Input placeholder="Password" type="password" />
+            <Button>Log in</Button>
+        </TabsContent>
+        <TabsContent value="signup" className="flex flex-col gap-3">
+            <Input placeholder="Name" type="text" />
+            <Input placeholder="Email" type="email" />
+            <Input placeholder="Password" type="password" />
+            <Input placeholder="Confirm Password" type="password" />
+            <Button>Sign up</Button>
+        </TabsContent>
+      </Tabs>
     </div>
-  )
+  );
 };
 
-export default page
+export default page;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/theme-mode";
 import { buttonVariants } from "@/components/ui/button"
 import Link from "next/link";
 
@@ -5,7 +6,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Link href="/login" className={buttonVariants({ variant: "outline", class: "bg-black rounded text-white hover:text-black" })}>Go to Login Page</Link>
+      <Link href="/login" className={buttonVariants({ variant: "outline" })}>Go to Login Page</Link>
     </div>
   );
 }
