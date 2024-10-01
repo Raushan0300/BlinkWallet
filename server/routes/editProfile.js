e735425c3b0f1c15e6789689b36d1f2043f276ba
Raushan Kumar <raushankumar2909@gmail.com>
2024-10-01 23:34:49 +0530

    Refactor user info route and add edit profile functionality

--- server/routes/editProfile.js
+++ server/routes/editProfile.js
@@ -6,16 +6,22 @@ const {authenticatedToken} = require('../controller/authenticatedToken');
 
 const User = require('../models/Users');
 
-router.get('/', authenticatedToken, async(req, res)=>{
+router.post('/', authenticatedToken, async(req, res)=>{
+    const {name, email, dob} = req.body;
     try{
-        const user = await User.findById(req.user._id).select('-password -_id -__v');
+        const user = await User.findById(req.user._id);
         if(!user) return res.status(404).json({message: 'User Not Found'});
 
-        return res.status(200).json({user});
+        if(name) user.name = name;
+        if(email) user.email = email;
+        if(dob) user.dob = dob;
+
+        await user.save();
+
+        return res.status(200).json({message: 'User Updated Successfully'});
+
     } catch (error) {
         console.log(error);
         return res.status(500).json({message: 'Internal Server Error'});
     }
 });
-
-module.exports = router;
