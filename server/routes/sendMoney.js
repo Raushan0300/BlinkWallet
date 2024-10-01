7a1a6b15350889b9127ce20519fb99eda4126e44
Raushan Kumar <raushankumar2909@gmail.com>
2024-10-01 22:54:15 +0530

    Refactor error response status code in sendMoney route

--- server/routes/sendMoney.js
+++ server/routes/sendMoney.js
@@ -19,7 +19,7 @@ router.post('/', authenticatedToken, async(req, res)=>{
         if(wallet.balance < amount) return res.status(400).json({message: 'Insufficient Balance'});
 
         const user = await User.findOne({email});
-        if(!user) return res.status(404).json({message: 'User Not Found'});
+        if(!user) return res.status(406).json({message: 'User Not Found'});
 
         const userWallet = await Wallet.findOne({user: user._id});
         if(!userWallet) return res.status(404).json({message: 'User Wallet Not Found'});
@@ -49,6 +49,7 @@ router.post('/', authenticatedToken, async(req, res)=>{
             date: Date.now()
         });
         await userHistory.save();
+        return res.status(200).json({balance:newBalance ,message: 'Money Sent Successfully'});
     } catch (error) {
         console.log(error);
         return res.status(500).json({message: 'Internal Server Error'});
